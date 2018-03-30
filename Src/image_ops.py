442ca06f0fef4f328d2aed2e5e9025d2543e85cf
Kouame <kouameh@hotmail.com>
2018-03-30 17:41:24 +0200

    coded rescaling and linear interpolation ops

--- Src/image_ops.py
+++ Src/image_ops.py
@@ -17,12 +17,22 @@ def save(image, path='../Output/out.jpg'):
     cv2.imwrite(path, image)
 
 
+def rescale(image_in, width = 128, height = 128):
+    # Bipolar interpolation: experiment with CV_INTER_AREA
+    image_out = cv2.resize(image_in, (width, height), 0, 0)
+    return image_out
+
+
 def main():
     image = load()
     display(image)
-    save(image)
+    rescaled_image = rescale(image)
+    display(rescaled_image)
+    save(image, '../Output/original.jpg')
+    save(rescaled_image, '../Output/rescaled.jpg')
 
 
 if __name__ == '__main__':
     if __name__ == '__main__':
         main()
+
