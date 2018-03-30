ea9937b2204a2c540d1dfe67fd5646867fb6d40e
Kouame <kouameh@hotmail.com>
2018-03-30 23:25:14 +0200

    rgb to c1, c2, c3 converter coded

--- Src/image_ops.py
+++ Src/image_ops.py
@@ -23,13 +23,37 @@ def rescale(image_in, width = 128, height = 128):
     return image_out
 
 
+def rgb_to_components(rgb_image, bits_per_pixel = 24):
+    max_pixel_value = pow(2, bits_per_pixel)
+    dimensions = rgb_image.shape
+    height, width = dimensions[0], dimensions[1]
+    components_image = np.zeros((3, height, width))
+    # C1[x][y] = components_image[0][x][y] = np.mean(rgb_image[x][y])
+    components_image[0][:][:] = np.mean(rgb_image[:][:], 2)
+    for x in range(height):
+        for y in range(width):
+            # components_image[0][x][y] = np.mean(rgb_image[x][y])
+            # C2[x][y]
+            components_image[1][x][y] = (rgb_image[x][y][0] + max_pixel_value - rgb_image[x][y][2]) / 2
+            # C3[x][y]
+            components_image[2][x][y] = (rgb_image[x][y][0] +
+                                         2 * (max_pixel_value - rgb_image[x][y][1]) +
+                                         rgb_image[x][y][2]) / 4
+    return components_image
+
+
 def main():
     image = load()
     display(image)
-    rescaled_image = rescale(image)
-    display(rescaled_image)
-    save(image, '../Output/original.jpg')
-    save(rescaled_image, '../Output/rescaled.jpg')
+    rescaled_image = rescale(image, 2, 2)
+    # print "rgb values"
+    # print rescaled_image
+    # components_image = rgb_to_components(rescaled_image)
+    # print "component values"
+    # print components_image
+    # display(rescaled_image)
+    # save(image, '../Output/original.jpg')
+    # save(rescaled_image, '../Output/rescaled.jpg')
 
 
 if __name__ == '__main__':
